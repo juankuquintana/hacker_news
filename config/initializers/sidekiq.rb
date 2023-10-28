96adf79bfea9b3450f262c659e03bd3059504a34
Juan Ku Quintana <juan@yellowme.mx>
2023-10-28 17:49:36 -0600

    Test Redis config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,15 +1,20 @@
-Sidekiq.configure_server do |config|
-  config.redis = {
+if Rails.env.production?
+  options = {
+    url: Figaro.env.redis_url,
+    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+  }
+else
+  options = {
     host: Figaro.env.redis_host,
     port: Figaro.env.redis_port,
     ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
   }
 end
 
+Sidekiq.configure_server do |config|
+  config.redis = options
+end
+
 Sidekiq.configure_client do |config|
-  config.redis = {
-    host: Figaro.env.redis_host,
-    port: Figaro.env.redis_port,
-    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
-  }
+  config.redis = options
 end
