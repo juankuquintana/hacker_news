01baf075a11913e105b802ab9549e2d427342b9f
Juan Ku Quintana <juan@yellowme.mx>
2023-10-28 17:53:49 -0600

    Test Redis config

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,5 @@
-$redis = Redis.new(host: Figaro.env.redis_host, port: Figaro.env.redis_port, ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
+if Rails.env.production?
+  $redis = Redis.new(url: Figaro.env.redis_url, ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
+else
+  $redis = Redis.new(host: Figaro.env.redis_host, port: Figaro.env.redis_port, ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
+end
